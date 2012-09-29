4e44e4e42da133e4079724b92ecb38c04fb1c0e4
Steven Yang <stevenzyang@berkeley.edu>
2012-09-29 15:36:28 -0700

    Steven's side

---
+++ bomb.py
@@ -0,0 +1,8 @@
+class Bomb:
+	def _init_(self, power, position):
+		self.power = power
+		self.time = 0
+
+
+	def explode():
+		
---
+++ explosion.py
@@ -0,0 +1,5 @@
+class explosion:
+
+	def _init_(self):
+		self.power = power
+		self.position = position
