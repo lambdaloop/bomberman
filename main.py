d96860bfe43d5a74257dc7722b8e56dc30886158
Pierre Karashchuk <krchtchk@gmail.com>
2012-09-30 02:02:59 -0700

    blah blah blha

--- main.py
+++ main.py
@@ -79,7 +79,6 @@ def update_explosions(t):
         e.update(t)
 
 def update_stuff(t):
-    #update explosions
     update_bombs(t)
     update_explosions(t)
 
@@ -96,15 +95,5 @@ while mainLoop:
     execute_AI()
     update_stuff(tickFPS)
     draw_stuff()
-
-    # ballrect = ballrect.move(speed)
-        # if ballrect.left < 0 or ballrect.right > width:
-        #     speed[0] = -speed[0]
-        # if ballrect.top < 0 or ballrect.bottom > height:
-        #     speed[1] = -speed[1]
-
-    # screen.fill(black)
-        # screen.blit(ball, ballrect)
-        # pygame.display.flip()
 
 sys.exit()
