ad947059f20d7ce5fc5535ef60830f26d4809147
stevenzyang <stevenzyang@berkeley.edu>
2012-11-04 18:42:13 -0800

    yeah

--- menu.py
+++
@@ -1,7 +0,0 @@
-import pygame
-from pygame.locals import *
-from bomber_constants.py import *
-from drawstuff.py import *
-
-font_path = 'coders_crux/coders_crux.ttf'
-font = pygame.font.Font(self.font_path, size = 32)
